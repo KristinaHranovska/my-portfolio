124017d6a10b67d3532de7e6ba93b3ffd37cbbc0
Kristina <kr.hranovska@gmail.com>
2024-08-07 13:48:39 +0300

    add certificates file

---
+++ src/shared/data/certificates.js
@@ -0,0 +1,26 @@
+import {
+    imagesCertificates
+} from 'shared/images';
+
+const certificates = [
+    {
+        img: `${imagesCertificates.fullstackgoit}`,
+        name: 'Fullstack Developer',
+        company: 'GoIT - start your career in IT',
+        date: 'July, 2024'
+    },
+    {
+        img: `${imagesCertificates.tsitvdn}`,
+        name: 'TypeScript',
+        company: 'ITVDN',
+        date: 'April, 2024'
+    },
+    {
+        img: `${imagesCertificates.jsfreecodecamp}`,
+        name: 'JavaScript Algorithms and Data Structures',
+        company: 'freeCodeCamp',
+        date: 'February, 2024'
+    }
+]
+
+export default certificates;
