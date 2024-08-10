bd9acd2256a8ba0e6e17f2b16cf9592cd677bbb9
Kristina <kr.hranovska@gmail.com>
2024-08-10 11:09:05 +0300

    add pages style

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,6 +1,6 @@
 import ContactsInform from 'modules/ContactsInform/ContactsInform';
 import MainInform from 'modules/MainInform/MainInform';
-import style from '../style';
+import style from '../pagesStyle.module.scss';
 
 const Blog = () => {
   return (
