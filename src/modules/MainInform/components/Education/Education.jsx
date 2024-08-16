3d64c98f98cc4e023520fc2e607662ca00e475a5
Kristina <kr.hranovska@gmail.com>
2024-08-16 21:00:02 +0300

    experience is ready

--- src/modules/MainInform/components/Education/Education.jsx
+++ src/modules/MainInform/components/Education/Education.jsx
@@ -3,7 +3,7 @@ import style from './Education.module.scss';
 
 const Education = () => {
   return (
-    <div>
+    <div className={style.timeline}>
       <div className={style.educationWrapper}>
         <div className={style.iconBox}>
           <svg className={style.iconBook} width="16" height="16">
